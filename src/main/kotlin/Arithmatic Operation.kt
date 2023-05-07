70d984a5e6c0ff8c81fa0cfe683fbab76f95055e
Varun Gupta <varunguptaalg@gmail.com>
2023-05-07 17:50:33 +0530

    First Commit

---
+++ src/main/kotlin/Arithmatic Operation.kt
@@ -0,0 +1,12 @@
+
+
+fun main(){
+    var x = 7
+    var y = 4
+    println("The value of x+y is: ${x+y}")
+
+    println("The value of x/y is: ${x/y}")
+    var a = 7F // OR var a:Float = 7F
+    var b = 4F
+    print("The value of a/b is: ${a/b}")
+}
