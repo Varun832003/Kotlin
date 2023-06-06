56e0ae936eda8584316b8b072f0975b03a297165
Varun Gupta <varunguptaalg@gmail.com>
2023-06-07 00:40:07 +0530

    my Commit

---
+++ src/main/kotlin/1_Tutorial.kt
@@ -0,0 +1,17 @@
+
+
+fun main(){
+    println("Hello World")
+    // Declaring a variable - method1
+    var myVariable1: Int = 83
+    println(myVariable1)
+
+    // method-2
+    var myVariable2 = 85
+    println("The value of myVariable is : $myVariable2")
+    // value of variable declared with var keyword can be changed .
+
+    val num = 23
+    // num=33 --> can't be done
+    // value of variable declared with val keyword can't be changed.
+}
